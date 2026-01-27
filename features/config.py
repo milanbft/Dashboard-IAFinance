3cb0e5e97b37485c56c902b01c5e8c93387cdcb3
milanbft <milan.briffaut@gmail.com>
2026-01-27 10:50:42 +0100

    Update API keys in config.py

--- features/config.py
+++ features/config.py
@@ -2,7 +2,7 @@
 
 import os
 
-ALPHA_VANTAGE_API_KEY = os.environ.get("QH192PUJ23K1C6Z8")
-QUANDL_API_KEY = os.environ.get("pX6EDwLFMK1zsMNsdxej")
+ALPHA_VANTAGE_API_KEY = os.environ.get("AV_KEY")
+QUANDL_API_KEY = os.environ.get("NASDAQ_KEY")
 
 DATA_DIR = "../data/raw"
